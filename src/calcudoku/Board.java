f535b245f923034474f342d190e6539d1f60616d
Enzo Altamiranda <enzo.alt@gmail.com>
2015-03-29 10:07:42 -0300

    Started work on board and block classes.

--- src/calcudoku/Board.java
+++ src/calcudoku/Board.java
@@ -1,7 +1,29 @@
 package calcudoku;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Enzo on 29.03.15.
  */
 public class Board {
+
+    public final int board_size;
+
+    private Map<Integer, Block> blocks;
+    private int block_num = 0;
+
+    public Board(int board_size) {
+        this.board_size = board_size;
+    }
+
+    public void addBlock(Block block) {
+
+
+
+    }
+
+    public void getBlockSolutions() {
+
+    }
 }
