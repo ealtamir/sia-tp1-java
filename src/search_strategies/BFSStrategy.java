d85d7cd6e4ebef2988252ed0ad0733087f0e9f05
contiver <cristianontivero@gmail.com>
2015-03-29 11:02:34 -0300

    strategies implementadas

--- src/search_strategies/BFSStrategy.java
+++ src/search_strategies/BFSStrategy.java
@@ -1,11 +1,25 @@
 package search_strategies;
 
 import engine.GPSNode;
-import search_strategies.SearchStrategy;
 
+import java.util.LinkedList;
+
+/* FIFO */
 public class BFSStrategy extends SearchStrategy {
+    private Cost cost;
+    private static final int INITIAL_CAPACITY = 16384;
+
+    public BFSStrategy(Cost cost){
+        this.cost = cost;
+        this.frontier = new LinkedList<GPSNode>();
+    }
+
+    private int evaluation(GPSNode node){
+        return cost.evaluate(node);
+    }
+
     @Override
     public GPSNode frontierPop() {
-        return null;
+        return ((LinkedList<GPSNode>) frontier).getFirst();
     }
 }
